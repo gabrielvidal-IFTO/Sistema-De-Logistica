1ec4056e7bfea511dbea9c8a2f7a4b47b157833d
Gabriel Vidal <vidalgabriel1324@gmail.com>
2026-06-09 16:42:50 -0300

    update banco de dados

--- dados.js
+++ dados.js
@@ -1,37 +1,32 @@
 const dadosRodoviarios = [
-  { "origem": "Gurupi, TO", "destino": "Dueré, TO", "distancia": "43.5 km", "tempo": "45 minutos", "degradacao": 7 },
+  
+  // --- EIXO PRINCIPAL: BR-153 (Degradação Baixa) ---
+ 
   { "origem": "Gurupi, TO", "destino": "Cariri do Tocantins, TO", "distancia": "17.1 km", "tempo": "15 minutos", "degradacao": 2 },
-  { "origem": "Gurupi, TO", "destino": "Formoso do Araguaia, TO", "distancia": "75.4 km", "tempo": "1 hora 10 minutos", "degradacao": 8 },
-  { "origem": "Gurupi, TO", "destino": "Alvorada, TO", "distancia": "73.2 km", "tempo": "55 minutos", "degradacao": 4 },
-  { "origem": "Gurupi, TO", "destino": "Figueirópolis, TO", "distancia": "45.8 km", "tempo": "35 minutos", "degradacao": 4 },
-
-  { "origem": "Dueré, TO", "destino": "Gurupi, TO", "distancia": "43.5 km", "tempo": "45 minutos", "degradacao": 7 },
-  { "origem": "Dueré, TO", "destino": "Cariri do Tocantins, TO", "distancia": "60.2 km", "tempo": "55 minutos", "degradacao": 8 },
-  { "origem": "Dueré, TO", "destino": "Formoso do Araguaia, TO", "distancia": "68.1 km", "tempo": "1 hora", "degradacao": 9 },
-  { "origem": "Dueré, TO", "destino": "Alvorada, TO", "distancia": "116.0 km", "tempo": "1 hora 35 minutos", "degradacao": 8 },
-  { "origem": "Dueré, TO", "destino": "Figueirópolis, TO", "distancia": "88.5 km", "tempo": "1 hora 15 minutos", "degradacao": 7 },
-
   { "origem": "Cariri do Tocantins, TO", "destino": "Gurupi, TO", "distancia": "17.1 km", "tempo": "15 minutos", "degradacao": 2 },
-  { "origem": "Cariri do Tocantins, TO", "destino": "Dueré, TO", "distancia": "60.2 km", "tempo": "55 minutos", "degradacao": 8 },
-  { "origem": "Cariri do Tocantins, TO", "destino": "Formoso do Araguaia, TO", "distancia": "92.5 km", "tempo": "1 hora 20 minutos", "degradacao": 7 },
-  { "origem": "Cariri do Tocantins, TO", "destino": "Alvorada, TO", "distancia": "56.4 km", "tempo": "40 minutos", "degradacao": 3 },
-  { "origem": "Cariri do Tocantins, TO", "destino": "Figueirópolis, TO", "distancia": "29.1 km", "tempo": "22 minutos", "degradacao": 3 },
 
-  { "origem": "Formoso do Araguaia, TO", "destino": "Gurupi, TO", "distancia": "75.4 km", "tempo": "1 hora 10 minutos", "degradacao": 8 },
-  { "origem": "Formoso do Araguaia, TO", "destino": "Dueré, TO", "distancia": "68.1 km", "tempo": "1 hora", "degradacao": 9 },
-  { "origem": "Formoso do Araguaia, TO", "destino": "Cariri do Tocantins, TO", "distancia": "92.5 km", "tempo": "1 hora 20 minutos", "degradacao": 7 },
-  { "origem": "Formoso do Araguaia, TO", "destino": "Alvorada, TO", "distancia": "148.0 km", "tempo": "2 horas", "degradacao": 9 },
-  { "origem": "Formoso do Araguaia, TO", "destino": "Figueirópolis, TO", "distancia": "121.0 km", "tempo": "1 hora 40 minutos", "degradacao": 8 },
+  { "origem": "Cariri do Tocantins, TO", "destino": "Figueirópolis, TO", "distancia": "31.8 km", "tempo": "34 minutos", "degradacao": 3 },
+  { "origem": "Figueirópolis, TO", "destino": "Cariri do Tocantins, TO", "distancia": "31.8 km", "tempo": "34 minutos", "degradacao": 3 },
 
-  { "origem": "Alvorada, TO", "destino": "Gurupi, TO", "distancia": "73.2 km", "tempo": "55 minutos", "degradacao": 4 },
-  { "origem": "Alvorada, TO", "destino": "Dueré, TO", "distancia": "116.0 km", "tempo": "1 hora 35 minutos", "degradacao": 8 },
-  { "origem": "Alvorada, TO", "destino": "Cariri do Tocantins, TO", "distancia": "56.4 km", "tempo": "40 minutos", "degradacao": 3 },
-  { "origem": "Alvorada, TO", "destino": "Formoso do Araguaia, TO", "distancia": "148.0 km", "tempo": "2 horas", "degradacao": 9 },
-  { "origem": "Alvorada, TO", "destino": "Figueirópolis, TO", "distancia": "27.5 km", "tempo": "20 minutos", "degradacao": 5 },
+  { "origem": "Figueirópolis, TO", "destino": "Alvorada, TO", "distancia": "44 km", "tempo": "39 minutos", "degradacao": 4 },
+  { "origem": "Alvorada, TO", "destino": "Figueirópolis, TO", "distancia": "44 km", "tempo": "39 minutos", "degradacao": 4 },
 
-  { "origem": "Figueirópolis, TO", "destino": "Gurupi, TO", "distancia": "45.8 km", "tempo": "35 minutos", "degradacao": 4 },
-  { "origem": "Figueirópolis, TO", "destino": "Dueré, TO", "distancia": "88.5 km", "tempo": "1 hora 15 minutos", "degradacao": 7 },
-  { "origem": "Figueirópolis, TO", "destino": "Cariri do Tocantins, TO", "distancia": "29.1 km", "tempo": "22 minutos", "degradacao": 3 },
-  { "origem": "Figueirópolis, TO", "destino": "Formoso do Araguaia, TO", "distancia": "121.0 km", "tempo": "1 hora 40 minutos", "degradacao": 8 },
-  { "origem": "Figueirópolis, TO", "destino": "Alvorada, TO", "distancia": "27.5 km", "tempo": "20 minutos", "degradacao": 5 }
+
+  
+  // --- EIXO SECUNDÁRIO: RODOVIAS ESTADUAIS ( Degradação Alta) ---
+  
+  
+  // 1. Conexões de Dueré, TO
+  { "origem": "Gurupi, TO", "destino": "Dueré, TO", "distancia": "43.5 km", "tempo": "45 minutos", "degradacao": 7 },
+  { "origem": "Dueré, TO", "destino": "Gurupi, TO", "distancia": "43.5 km", "tempo": "45 minutos", "degradacao": 7 },
+
+  { "origem": "Dueré, TO", "destino": "Formoso do Araguaia, TO", "distancia": "65.5 km", "tempo": "50 minutos", "degradacao": 8 },
+  { "origem": "Formoso do Araguaia, TO", "destino": "Dueré, TO", "distancia": "65.5 km", "tempo": "50 minutos", "degradacao": 8 },
+
+  // 2. Conexões de Formoso do Araguaia, TO 
+  { "origem": "Formoso do Araguaia, TO", "destino": "Cariri do Tocantins, TO", "distancia": "75.4 km", "tempo": "1 hora 10 minutos", "degradacao": 7 },
+  { "origem": "Cariri do Tocantins, TO", "destino": "Formoso do Araguaia, TO", "distancia": "75.4 km", "tempo": "1 hora 10 minutos", "degradacao": 7 },
+
+  { "origem": "Formoso do Araguaia, TO", "destino": "Figueirópolis, TO", "distancia": "65.9 km", "tempo": "49 minutos", "degradacao": 8 },
+  { "origem": "Figueirópolis, TO", "destino": "Formoso do Araguaia, TO", "distancia": "65.9 km", "tempo": "49 minutos", "degradacao": 8 }
 ];
